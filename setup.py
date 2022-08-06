4b8eed1ad2f8477ff89fe021caf8367a56abedf8
msr8 <live.shashwat@gmail.com>
2022-08-06 20:17:06 +0530

    version 0.0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools  import setup
 
 
 with open('README.md') as f:
-    long_desc = f.read()
+    a = f.read()
 
 # with open('VERSION.txt') as f:
 #     version = f.read()
@@ -16,18 +16,16 @@ with open('requirements.txt') as f:
 
 
 # This section to make the usage video compatible with PyPi
-old_vid_tag = '''\
-https://user-images.githubusercontent.com/79649185/182558272-255becc8-1dcc-45b5-99ef-22e0596cf490.mp4
-'''
-new_vid_tag = '''\
+old = 'https://user-images.githubusercontent.com/79649185/182558272-255becc8-1dcc-45b5-99ef-22e0596cf490.mp4'
+new = '''\
 <video controls> 
     <source src='https://raw.githubusercontent.com/msr8/markify/main/ass/usagelol.mp4' type="video/mp4">lol
-</video>
+</video>\
 '''
-long_desc.replace(old_vid_tag, new_vid_tag)
+long_desc = a.replace(old, new)
 
-
-
+# print(long_desc[:3600])
+# print(a)
 
 
 setup(
